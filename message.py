335aca3058b3edb167fcc4fa21b21e1d6d5bab29
Jerry Wang <kuanchieh.w@gmail.com>
2021-09-16 08:47:21 +0900

    add word count

--- message.py
+++ message.py
@@ -9,23 +9,54 @@ with open('reviews.txt', 'r') as f:
 
 print('We have read all the data, totally', len(data), 'files')
 
-sum_len = 0
+print(data[0])
+
+wc = {} #word_count
 for d in data:
-	sum_len = sum_len + len(d)
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] +=1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
 
-print('average length is', sum_len/len(data))
+print(len(wc))
+print(wc['Allen'])
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('totally', len(new), 'files have less than 100 words')
-print(new[0])
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為:', wc[word])
+	else:
+		print('這個字沒有出現過喔')
 
+print('感謝使用本功能')
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
-print('totally', len(good), 'messages have good inside')
-print(good[0])
+#sum_len = 0
+#for d in data:
+#sum_len = sum_len + len(d)
+#
+#print('average length is', sum_len/len(data))
+#
+#new = []
+#for d in data:
+#if len(d) < 100:
+#new.append(d)
+#print('totally', len(new), 'files have less than 100 words')
+#print(new[0])
+#
+#
+#
+#good = []
+#for d in data:
+#if 'good' in d:
+#good.append(d)
+#print('totally', len(good), 'messages have good inside')
+#print(good[0])
+#
+#good = [d for d in data if 'good' in d]
